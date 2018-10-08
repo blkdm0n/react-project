b6ff4a289873d44dd4d7de034c1728f9e41c0c86
Bernard Kitchens <bernard.kitchens@gmail.com>
2018-10-08 16:20:34 -0700

    Refactored App component from functional, stateless to a class component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,55 @@
-
-const Pet = (props) => {
+const Pet = props => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, props.name),
     React.createElement("h2", {}, props.animal),
     React.createElement("h2", {}, props.breed)
   ]);
 };
-const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet),
-    React.createElement(Pet),
-    React.createElement(Pet)
-  ]);
-};
 
+//  FUNCTIONAL, STATELESS
+// const App = () => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", {}, "Adopt Me!"),
+//     React.createElement(Pet, {
+//       name: "Marlowe",
+//       animal: "Dog",
+//       breed: "Labradoodle"
+//     }),
+//     React.createElement(Pet, {
+//       name: "Homer",
+//       animal: "Lizard",
+//       breed: "Asian Water Monitor"
+//     }),
+//     React.createElement(Pet, {
+//       name: "Hashimoto",
+//       animal: "Snake",
+//       breed: "King Cobra"
+//     })
+//   ]);
+// };
+
+//  CLASS COMPONENT
+
+class App extends React.Component {
+  render() {
+    return React.createElement("div", {}, [
+      React.createElement("h1", {}, "Adopt Me!"),
+      React.createElement(Pet, {
+        name: "Marlowe",
+        animal: "Dog",
+        breed: "Labradoodle"
+      }),
+      React.createElement(Pet, {
+        name: "Homer",
+        animal: "Lizard",
+        breed: "Asian Water Monitor"
+      }),
+      React.createElement(Pet, {
+        name: "Hashimoto",
+        animal: "Snake",
+        breed: "King Cobra"
+      })
+    ]);
+  }
+}
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
