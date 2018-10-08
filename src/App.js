2a5a3a526371b2d06021d720f3a70f6b27b00d1d
Bernard Kitchens <bernard.kitchens@gmail.com>
2018-10-08 16:09:50 -0700

    made Pet component reusable by adding props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-const Pet = () => {
+
+const Pet = (props) => {
   return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Marlowe"),
-    React.createElement("h2", {}, "Dog"),
-    React.createElement("h2", {}, "Labradoodle")
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed)
   ]);
 };
 const App = () => {
