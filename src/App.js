8abef8bbe596f6f53ca24a308cef050369330031
Bernard Kitchens <bernard.kitchens@gmail.com>
2018-10-08 16:07:16 -0700

    moved App component to separate js file and added style.css

---
+++ src/App.js
@@ -0,0 +1,17 @@
+const Pet = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Marlowe"),
+    React.createElement("h2", {}, "Dog"),
+    React.createElement("h2", {}, "Labradoodle")
+  ]);
+};
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet),
+    React.createElement(Pet),
+    React.createElement(Pet)
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
