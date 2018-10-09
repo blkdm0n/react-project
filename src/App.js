61d926318f0f0201669c1392fd509b77b8cde0a9
Bernard Kitchens <bernard.kitchens@gmail.com>
2018-10-08 17:43:57 -0700

    moved Pet component to its own file, created a package.json for dependencies

--- src/App.js
+++ src/App.js
@@ -1,39 +1,14 @@
-const Pet = props => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed)
-  ]);
-};
-
-//  FUNCTIONAL, STATELESS
-// const App = () => {
-//   return React.createElement("div", {}, [
-//     React.createElement("h1", {}, "Adopt Me!"),
-//     React.createElement(Pet, {
-//       name: "Marlowe",
-//       animal: "Dog",
-//       breed: "Labradoodle"
-//     }),
-//     React.createElement(Pet, {
-//       name: "Homer",
-//       animal: "Lizard",
-//       breed: "Asian Water Monitor"
-//     }),
-//     React.createElement(Pet, {
-//       name: "Hashimoto",
-//       animal: "Snake",
-//       breed: "King Cobra"
-//     })
-//   ]);
-// };
-
-//  CLASS COMPONENT
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
 
 class App extends React.Component {
+  handleTitleClick() {
+    alert("The title was clicked!");
+  }
   render() {
     return React.createElement("div", {}, [
-      React.createElement("h1", {}, "Adopt Me!"),
+      React.createElement("h1", { onClick: this.handleTitleClick }, "Adopt Me!"),
       React.createElement(Pet, {
         name: "Marlowe",
         animal: "Dog",
